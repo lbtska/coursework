2af9e1b719748b9bd5ea114a97a7951f2799cfe4
lbtska <labetska.lilichka@gmail.com>
2024-12-05 01:18:30 +0200

    Add files via upload

---
+++ coursework/coursework/script.js
@@ -0,0 +1,11 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+t.getElementById("name").value;
+    const service = document.getElementById("service").value;
+    const date = document.getElementById("date").value;
+
+    document.getElementById("confirmation-message").style.display = "block";
+
+    document.getElementById("registration-form").reset();
+});
